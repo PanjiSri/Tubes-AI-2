0cf82a63a330dd5055fc0f0b0c3a91746d75b6d3
banaman <ahmadalbana98@gmail.com>
2024-12-14 18:12:35 +0700

    feat: implement gain_ratio and handle empty examples

--- src/ID3.py
+++ src/ID3.py
@@ -4,6 +4,7 @@ import pandas as pd
 class ID3Scratch:
     def __init__(self, num_cols=[]):
         self.dict_num_bound = dict.fromkeys(num_cols)
+        self.dict_cat = {}
         self.tree = {}
 
     def fit(self, X, y):
@@ -13,11 +14,26 @@ class ID3Scratch:
         for col in self.dict_num_bound.keys():
             data = self.discretize_numerical_feature(data, col)
 
+        for col in data.columns[:-1]:
+            self.dict_cat[col] = data[col].unique()
+
         self.tree = self._build_tree(data)
     
     def predict(self, X):
         predictions = X.apply(lambda row: self._classify(row, self.tree), axis=1)
         return predictions
+
+    def _get_rules(self):
+        rules = []
+        def traverse(node, rule=[]):
+            if not isinstance(node, dict):
+                rules.append(rule + [node])
+            else:
+                feature = next(iter(node))
+                for value in node[feature]:
+                    traverse(node[feature][value], rule + [feature + '=' + value])
+        traverse(self.tree)
+        return rules
 
     def _entropy(self, data):
         labels = data['label']
@@ -37,13 +53,16 @@ class ID3Scratch:
     
     def _gain_ratio(self, data, feature):
         info_gain = self._information_gain(data, feature)
-        split_info = self._entropy(data[feature])
+        split_info = self._entropy(data[[feature, 'label']])
         return info_gain / split_info
 
     def _best_feature(self, data):
         features = data.columns[:-1]  # Exclude the target column
         info_gains = {feature: self._information_gain(data, feature) for feature in features}
-        # NOT YET IMPLEMENTED GAIN RATIO
+        avg_gain = sum(info_gains.values()) / len(info_gains)
+        for feature in info_gains:
+            if info_gains[feature] > avg_gain:
+                info_gains[feature] = self._gain_ratio(data, feature)
         return max(info_gains, key=info_gains.get)
 
     def _build_tree(self, data, tree=None):
@@ -55,10 +74,13 @@ class ID3Scratch:
         
         best_feature = self._best_feature(data)
         tree = {best_feature: {}}
-        for value in data[best_feature].unique():
+        for value in self.dict_cat[best_feature]:
             subset = data[data[best_feature] == value].drop(columns=[best_feature])
-            subtree = self._build_tree(subset)
-            tree[best_feature][value] = subtree
+            if (subset.empty):
+                tree[best_feature][value] = data['label'].mode()[0]
+            else:
+                subtree = self._build_tree(subset)
+                tree[best_feature][value] = subtree
         return tree
 
     def _classify(self, row, tree):
@@ -115,7 +137,7 @@ data = {
     'Outlook': ['Sunny', 'Sunny', 'Overcast', 'Rain', 'Rain', 'Rain', 'Overcast', 'Sunny', 'Sunny', 'Rain', 'Sunny', 'Overcast', 'Overcast', 'Rain'],
     'Temperature': ['Hot', 'Hot', 'Hot', 'Mild', 'Cool', 'Cool', 'Cool', 'Mild', 'Cool', 'Mild', 'Mild', 'Mild', 'Hot', 'Mild'],
     'Humidity': ['High', 'High', 'High', 'High', 'Normal', 'Normal', 'Normal', 'High', 'Normal', 'Normal', 'Normal', 'High', 'Normal', 'High'],
-    'Angka': [13, 15, 13, 10, 13, 15, 15, 13, 13, 13, 19, 15, 13, 15],
+    'Angka': [13, 15, 19, 10, 19, 15, 19, 14, 14, 14, 14, 15, 13, 15],
     'PlayTennis': ['No', 'No', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'No', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No']
 }
 
